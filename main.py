e73ac26d7108583b748e73d2f8c16bf1cc5c7a6d
Ariel Chouminov <31148540+arieldeveloper@users.noreply.github.com>
2018-04-18 22:57:05 -0400

    Added live graph and menu bar items
    Creates a live price chart containing current buy and sell information for Bitcoin. Also added the foundation for the menu bar items.

--- main.py
+++ main.py
@@ -1,10 +1,356 @@
+#Ariel Chouminov
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
+from matplotlib import pyplot as plt
+
+
+import urllib
+import json
+import pandas as pd
+import numpy as np
+pd.options.mode.chained_assignment = None
+
 #Import Tkinter
 import tkinter as tk
 from tkinter import ttk
 
+#Fonts
 LARGE_FONT = ("Verdana", 12)
+NORM_FONT = ("Verdana", 12)
+SMALL_FONT = ("Verdana", 12)
 
-#Main
+#Styling the Graph
+style.use("ggplot")
+f = Figure()
+a = f.add_subplot(111)
+
+#Default values
+exchange = "BTC-e"
+counter = 9000
+programName = "btce"
+resampleSize = "15Min"
+
+dataPace = "1d"
+candleWidth = 0.008
+
+topIndicator = "none"
+bottomIndicator = "none"
+middleIndicators = "none"
+EMAs = []
+SMAs = []
+
+chartLoad = True
+
+def loadChart(action):
+    global chartLoad
+
+    if action == "start":
+        chartLoad = True
+
+    if action == "stop":
+        chartLoad = False
+
+def tutorial():
+    pass
+
+def addMiddleIndicator(what):
+    global middleIndicators
+    global DatCounter
+    if dataPace == "tick":
+        popupmsg("Indicators in Tick Data not available, choose 1 minute tf if you want short term.")
+
+    if what != "none":
+        if middleIndicators == "none":
+
+            if what == "sma":
+                midIQ = tk.Tk()
+                midIQ.wm_title("Periods?")
+                label = ttk.Label(midIQ,
+                                  text="Choose how many periods you want each SMA calculation to consider.\nThese periods are contingent on your current time settings on the chart.\n1 period = 1 OHLC candlestick.",
+                                  font=NORM_FONT)
+                label.pack(side="top", fill="x", pady=10)
+                e = ttk.Entry(midIQ)
+                e.insert(0, 10)
+                e.pack()
+                e.focus_set()
+
+                def callback():
+                    middleIndicators = []
+                    periods = (e.get())
+                    group = []
+                    group.append("sma")
+                    group.append(int(periods))
+                    middleIndicators.append(group)
+                    DatCounter = 9000
+                    print("mid indicator", middleIndicators)
+                    midIQ.destroy()
+
+                b = ttk.Button(midIQ, text="Submit", width=10, command=callback)
+                b.pack()
+                tk.mainloop()
+
+            if what == "ema":
+                midIQ = tk.Tk()
+                midIQ.wm_title("Periods?")
+                label = ttk.Label(midIQ,
+                                  text="Choose how many periods you want each EMA calculation to consider.\nThese periods are contingent on your current time settings on the chart.\n1 period = 1 OHLC candlestick.",
+                                  font=NORM_FONT)
+                label.pack(side="top", fill="x", pady=10)
+                e = ttk.Entry(midIQ)
+                e.insert(0, 10)
+                e.pack()
+                e.focus_set()
+
+                def callback():
+                    middleIndicators = []
+                    periods = (e.get())
+                    group = []
+                    group.append("ema")
+                    group.append(int(periods))
+                    middleIndicators.append(group)
+                    DatCounter = 9000
+                    print("mid indicator", middleIndicators)
+                    midIQ.destroy()
+
+                b = ttk.Button(midIQ, text="Submit", width=10, command=callback)
+                b.pack()
+                tk.mainloop()
+
+
+        else:
+            if what == "sma":
+                midIQ = tk.Tk()
+                midIQ.wm_title("Periods?")
+                label = ttk.Label(midIQ,
+                                  text="Choose how many periods you want each SMA calculation to consider.\nThese periods are contingent on your current time settings on the chart.\n1 period = 1 OHLC candlestick.",
+                                  font=NORM_FONT)
+                label.pack(side="top", fill="x", pady=10)
+                e = ttk.Entry(midIQ)
+                e.insert(0, 10)
+                e.pack()
+                e.focus_set()
+
+                def callback():
+                    periods = (e.get())
+                    group = []
+                    group.append("sma")
+                    group.append(int(periods))
+                    middleIndicators.append(group)
+                    DatCounter = 9000
+                    print("mid indicator", middleIndicators)
+                    midIQ.destroy()
+
+                b = ttk.Button(midIQ, text="Submit", width=10, command=callback)
+                b.pack()
+                tk.mainloop()
+
+            if what == "ema":
+                midIQ = tk.Tk()
+                midIQ.wm_title("Periods?")
+                label = ttk.Label(midIQ,
+                                  text="Choose how many periods you want each EMA calculation to consider.\nThese periods are contingent on your current time settings on the chart.\n1 period = 1 OHLC candlestick.",
+                                  font=NORM_FONT)
+                label.pack(side="top", fill="x", pady=10)
+                e = ttk.Entry(midIQ)
+                e.insert(0, 10)
+                e.pack()
+                e.focus_set()
+
+                def callback():
+                    periods = (e.get())
+                    group = []
+                    group.append("ema")
+                    group.append(int(periods))
+                    middleIndicators.append(group)
+                    DatCounter = 9000
+                    print("mid indicator", middleIndicators)
+                    midIQ.destroy()
+
+                b = ttk.Button(midIQ, text="Submit", width=10, command=callback)
+                b.pack()
+                tk.mainloop()
+    else:
+        middleIndicators = "none"
+
+def addTopIndicator(what):
+    global topIndicator
+    global counter
+    global dataPace
+
+    if dataPace == "tick":
+        popupmsg("Indicators in tick data not available")
+
+    elif what == "none":
+        topIndicator = what
+        counter = 9000
+
+    elif what == "rsi":
+        rsiQuestion = tk.Tk()
+        rsiQuestion.wm_title("Periods?")
+        label = ttk.Label(rsiQuestion, text = "Choose how many periods you want each RSI calculation to consider.")
+        label.pack(side = "top", fill = "x", pady = 10)
+
+        e = ttk.Entry(rsiQuestion)
+        e.insert(0, 14)
+        e.pack()
+        e.focus_set()
+
+        def callBack():
+            global topIndicator
+            global counter
+
+            periods = (e.get())
+            group = []
+            group.append("rsi")
+            group.append(periods)
+
+            topIndicator = group
+            counter = 9000
+            print("Set top Indicator to", group)
+            rsiQuestion.destroy()
+
+        b = ttk.Button(rsiQuestion, text = "Submit", width = 10, command = callBack)
+        b.pack()
+        tk.mainloop()
+
+    elif what == "macd":
+
+        topIndicator = "macd"
+        counter = 9000
+
+
+def addBottomIndicator(what):
+    global bottomIndicator
+    global counter
+    global dataPace
+
+    if dataPace == "tick":
+        popupmsg("Indicators in tick data not available")
+
+    elif what == "none":
+        bottomIndicator = what
+        counter = 9000
+
+    elif what == "rsi":
+        rsiQuestion = tk.Tk()
+        rsiQuestion.wm_title("Periods?")
+        label = ttk.Label(rsiQuestion, text = "Choose how many periods you want each RSI calculation to consider.")
+        label.pack(side = "top", fill = "x", pady = 10)
+
+        e = ttk.Entry(rsiQuestion)
+        e.insert(0, 14)
+        e.pack()
+        e.focus_set()
+
+        def callBack():
+            global bottomIndicator
+            global counter
+
+            periods = (e.get())
+            group = []
+            group.append("rsi")
+            group.append(periods)
+
+            bottomIndicator = group
+            counter = 9000
+            print("Set bottom Indicator to", group)
+            rsiQuestion.destroy()
+
+        b = ttk.Button(rsiQuestion, text = "Submit", width = 10, command = callBack)
+        b.pack()
+        tk.mainloop()
+
+    elif what == "macd":
+        bottomIndicator = "macd"
+        counter = 9000
+
+
+def changeTimeFrame(tf):
+    global dataPace
+    global counter
+
+    if tf == "7d" and resampleSize == "1Min":
+        popupmsg("Too Much Data Chose, choose a smaller time frame or higher OHLC interval.")
+    else:
+        dataPace = tf
+        counter = 9000
+
+def changeSampleSize(size, width):
+    global resampleSize
+    global counter
+    global candleWidth
+
+    if dataPace == "7d" and resampleSize == "1Min":
+        popupmsg("Too Much Data Chose, choose a smaller time frame or higher OHLC interval.")
+
+    elif dataPace == "tick":
+        popupmsg("You're currently viewing tick data, not Open High low close")
+
+    else:
+        resampleSize = resampleSize
+        counter = 9000
+        candleWidth = width
+
+
+def changeExchange(toWhat, pn):
+    global exchange
+    global counter
+    global programName
+
+    exchange = toWhat
+    programName = pn
+    counter = 9000
+
+
+#Creates a pop up message with given text
+def popupmsg(msg):
+    popup = tk.Tk()
+
+    popup.wm_title("!")
+    label = ttk.Label(popup, text = msg, font = NORM_FONT)
+    label.pack(side = 'top', fill = "x", pady = 10)
+
+    #Closes the popup window
+    B1 = ttk.Button(popup, text = "Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+#Creates live graph
+def animate(i):
+    dataLink = 'https://wex.nz/api/3/trades/btc_usd?limit=2000'
+    data = urllib.request.urlopen(dataLink)
+
+    data = data.read().decode("utf-8")
+
+    data = json.loads(data)
+    #Takes all all the data under 'btc_usd' in the api
+    data = data["btc_usd"]
+
+    #Makes data a panda dataset
+    data = pd.DataFrame(data)
+
+    buys = data[(data['type'] == 'bid')]
+    buys["datestamp"] = np.array(buys["timestamp"]).astype("datetime64[s]")
+    buyDates = (buys["datestamp"]).tolist()
+
+    sells = data[(data['type'] == 'ask')]
+    sells["datestamp"] = np.array(sells["timestamp"]).astype("datetime64[s]")
+    sellDates = (sells["datestamp"]).tolist()
+
+    a.clear()
+    a.plot_date(buyDates, buys["price"], "#00A3E0", label = "buys")
+    a.plot_date(sellDates, sells["price"], "#183A54", label = "sells")
+
+    a.legend(bbox_to_anchor = (0, 1.02, 1, .102), loc = 3, ncol =2, borderaxespad = 0)
+    a.set_title("BTC Prices (USD) \nLast Price: "+str(data["price"][1999]))
+
+
+#Main class
 class BitcoinTracker(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -13,41 +359,133 @@ class BitcoinTracker(tk.Tk):
 
         #tk.Tk.iconbitmap(self, default = "icon.ico")
         tk.Tk.wm_title(self, "bitcoin tracker")
-        container = tk.Frame(self)
-        container.pack(side = "right", fill = "both", expand = True)
-        container.grid_rowconfigure(0, weight = 1)
-        container.grid_columnconfigure(0, weight = 1)
+        window = tk.Frame(self)
+        window.pack(side = "right", fill = "both", expand = True)
+        window.grid_rowconfigure(0, weight = 1)
+        window.grid_columnconfigure(0, weight = 1)
+
+        #Menu bar items
+        menubar = tk.Menu(window)
+        filemenu = tk.Menu(menubar, tearoff = 0)
+        filemenu.add_command(label = "Save Settings", command = lambda: popupmsg("Not Supported just yet!"))
+        filemenu.add_separator()
+        filemenu.add_command(label = "Exit", command = quit)
+
+        menubar.add_cascade(label = "File", menu = filemenu)
+
+        exchangeChoice = tk.Menu(menubar, tearoff = 1)
+
+        exchangeChoice.add_command(label= "BTC-e", command = lambda: changeExchange("BTC-e", "btce"))
+        exchangeChoice.add_command(label="Bitfinex", command=lambda: changeExchange("Bitfinex", "bitfinex"))
+        exchangeChoice.add_command(label="Bitstamp", command=lambda: changeExchange("Bitstamp", "bitstamp"))
+        exchangeChoice.add_command(label="Huobi", command=lambda: changeExchange("Huobi", "huobi"))
+
+        menubar.add_cascade(label = "Exchange", menu = exchangeChoice)
+
+        #Data time frame
+        dataTF = tk.Menu(menubar, tearoff = 1)
+        dataTF.add_command(label = "Tick", command = lambda: changeTimeFrame('tick'))
+        dataTF.add_command(label= "1 Day", command=lambda: changeTimeFrame('1d'))
+        dataTF.add_command(label= "3 Day", command=lambda: changeTimeFrame('3d'))
+        dataTF.add_command(label= "1 Week", command=lambda: changeTimeFrame('7d'))
+        menubar.add_cascade(label = "Data Time Frame", menu = dataTF)
+
+
+        OHLCI = tk.Menu(menubar, tearoff = 1)
+        OHLCI.add_command(label = "Tick", command = lambda: changeTimeFrame('tick'))
+        OHLCI.add_command(label = "1 Minute", command=lambda: changeSampleSize('1min', 0.0005))
+        OHLCI.add_command(label = "5 Minute", command=lambda: changeSampleSize('5min', 0.003))
+        OHLCI.add_command(label = "15 Minute", command=lambda: changeSampleSize('15min', 0.008))
+        OHLCI.add_command(label = "30 Minute", command=lambda: changeSampleSize('30min', 0.016))
+        OHLCI.add_command(label = "1 Hour", command=lambda: changeSampleSize('1H', 0.032))
+        OHLCI.add_command(label = "3 Hour", command=lambda: changeSampleSize('3H', 0.096))
+
+        menubar.add_cascade(label = "OHLC interval", menu = OHLCI)
+
+        topIndi = tk.Menu(menubar, tearoff = 1)
+        topIndi.add_command(label = "None", command = lambda: addTopIndicator('none'))
+        topIndi.add_command(label ="RSI", command=lambda: addTopIndicator('rsi'))
+        topIndi.add_command(label ="MACD", command=lambda: addTopIndicator('macd'))
+
+        menubar.add_cascade(label = "Top Indicator", menu = topIndi)
+
+
+        mainI = tk.Menu(menubar, tearoff = 1)
+        mainI.add_command(label = "None", command = lambda: addMiddleIndicator('none'))
+        mainI.add_command(label ="SMA", command=lambda: addMiddleIndicator('sma'))
+        mainI.add_command(label = "EMA", command=lambda: addMiddleIndicator('ema'))
+
+        menubar.add_cascade(label = "Main/middle Indicator", menu = mainI)
+
+
+        bottomI = tk.Menu(menubar, tearoff = 1)
+        bottomI.add_command(label = "None", command = lambda: addBottomIndicator('none'))
+        bottomI.add_command(label = "RSI", command=lambda: addBottomIndicator('rsi'))
+        bottomI.add_command(label = "MACD", command=lambda: addBottomIndicator('macd'))
+
+        menubar.add_cascade(label = "Bottom Indicator", menu = bottomI)
+
+        tradeButton = tk.Menu(menubar, tearoff = 1)
+        tradeButton.add_command(label = "Manual Trading", command = lambda: popupmsg('This is not live yet'))
+        tradeButton.add_command(label="Automated Trading", command=lambda: popupmsg('This is not live yet'))
+
+        tradeButton.add_separator()
+        tradeButton.add_command(label = "Quick Buy", command = lambda: popupmsg('This is not live yet'))
+        tradeButton.add_command(label="Quick Sell", command=lambda: popupmsg('This is not live yet'))
+        tradeButton.add_separator()
+        tradeButton.add_command(label="Set-up Quick Buy and Sell", command=lambda: popupmsg('This is not live yet'))
+
+        menubar.add_cascade(label = "trading", menu = tradeButton)
+
+        startStop = tk.Menu(menubar, tearoff = 1)
+        startStop.add_command(label = "resume", command = lambda: loadChart('start'))
+        startStop.add_command(label = "pause", command = lambda: loadChart('stop'))
+
+        menubar.add_cascade(label = "Resume/Pause client", menu = startStop)
+
+        helpMenu = tk.Menu(menubar, tearoff = 0)
+        helpMenu.add_command(label = "Tutorial", command = tutorial)
+
+        menubar.add_cascade(label = "Help", menu = helpMenu)
+
+
+        #sets candle wick size
+        tk.Tk.config(self, menu = menubar)
 
         self.frames = {}
 
-        for f in (StartPage, PageOne, PageTwo):
+        for f in (StartPage, Graph_Page):
 
-            frame = f(container, self)
-
+            frame = f(window, self)
             self.frames[f] = frame
-
             frame.grid(row = 0, column = 0, sticky = "nsew")
 
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
+        frame.tkraise() #Raises the frame to the front
 
-        #Raises the frame to the front
-        frame.tkraise()
-
+#Creates a start page asking if you agree or disagree
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
 
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text = "Start Page", font = LARGE_FONT)
+        label = ttk.Label(self, text = """Bitcoin Price Tracker is not responsible for any loss that
+         is caused because of our application.""", font = LARGE_FONT)
         label.pack(pady = 10, padx = 10)
 
-        button1 = ttk.Button(self, text = "Visit Page 1", command = lambda: controller.show_frame(PageOne))
+        #Takes you to the graph page
+        button1 = ttk.Button(self, text = "Agree", command = lambda: controller.show_frame(Graph_Page))
         button1.pack()
 
-class PageOne(tk.Frame):
+        #Exits the application
+        button2 = ttk.Button(self, text = "Disagree", command = quit)
+        button2.pack()
+
+"""
+class HomePage(tk.Frame):
     def __init__(self, parent, controller):
 
         tk.Frame.__init__(self, parent)
@@ -61,22 +499,29 @@ class PageOne(tk.Frame):
         button1.pack()
 
     tk.Frame
+"""
 
-class PageTwo(tk.Frame):
+class Graph_Page(tk.Frame):
     def __init__(self, parent, controller):
-
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text = "Page Two", font = LARGE_FONT)
-        label.pack(pady = 10, padx = 10)
+        label = ttk.Label(self, text="Graph Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
 
-
-        button1 = ttk.Button(self, text = "back to home", command = lambda: controller.show_frame(StartPage))
+        button1 = ttk.Button(self, text="back to home", command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = ttk.Button(self, text="To page 1", command=lambda: controller.show_frame(PageOne))
-        button2.pack()
 
-    tk.Frame
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side = tk.TOP, fill = tk.BOTH, expand = True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side = tk.TOP, fill = tk.BOTH, expand = True)
+        tk.Frame
 
 app = BitcoinTracker()
+#Set Screen size
+app.geometry("1280x720")
+ani = animation.FuncAnimation(f,animate, interval = 5000)
 app.mainloop()
